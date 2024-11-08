1b7d9f85f50c22a5c1eb8b0e2399fd4f6f16c222
francoperez03 <franco.perez03@gmail.com>
2024-11-08 14:45:58 -0300

    feat: add ContractList

--- frontend/src/pages/Contracts/ContractsList.tsx
+++ frontend/src/pages/Contracts/ContractsList.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getContracts } from '../../services/contractService';
-import { Contract } from '../../types/contract';
+import { ContractItem } from '../../types/contract';
 import ContractCard from '../../components/ContractCard/ContractCard';
+import './ContractsList.css';
 
 const ContractsList: React.FC = () => {
-  const [contracts, setContracts] = useState<Contract[]>([]);
+  const [contracts, setContracts] = useState<ContractItem[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -15,7 +16,7 @@ const ContractsList: React.FC = () => {
         const data = await getContracts();
         setContracts(data);
       } catch (err) {
-        console.error(err)
+        console.error(err);
         setError('Error al obtener los contratos.');
       } finally {
         setLoading(false);
@@ -29,13 +30,15 @@ const ContractsList: React.FC = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <div>
-      <h2>Lista de Contratos</h2>
-      <Link to="/contracts/add">Agregar Nuevo Contrato</Link>
+    <div className="contracts-list-container">
+      <h2 className="contracts-list-title">Lista de Contratos</h2>
+      <Link to="/contracts/add" className="add-contract-link">Agregar Nuevo Contrato</Link>
       <div className="contracts-list">
-        {contracts.map((contract) => (
-          <ContractCard key={contract.id} contract={contract} />
-        ))}
+        {contracts.map((contract) => {
+          console.log({contract})
+          contract.icon = `${contract.address}_icon.webp`
+          return <ContractCard key={contract.id} contract={contract} />
+        })}
       </div>
     </div>
   );
